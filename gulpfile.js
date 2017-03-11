8fc694efaa9ae8886c2b9ec3d2d4c9254928f0e6
Justin Carver <carver@justcarver.com>
2017-03-10 22:41:45 -0500

    Initial work on GulpFile and updated packages

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// Require Gulp
+const gulp = require('gulp');
+
+// Gulp Plugings
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const del = require('del');
+const hash = require('gulp-hash');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
