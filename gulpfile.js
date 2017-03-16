98bc255c1ca2c6ebf9afda6e0d3a002933b713c9
Justin Carver <carver@justcarver.com>
2017-03-15 22:56:48 -0400

    Added Gulp-Plumber and Font task to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const concat = require('gulp-concat');
 const del = require('del');
 const eslint = require('gulp-eslint');
 const hash = require('gulp-hash');
+const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
@@ -15,6 +16,7 @@ const uglify = require('gulp-uglify');
 
 gulp.task('lint', () => {
   return gulp.src('src/js/**/*.js')
+    .pipe(plumber())
     .pipe(eslint())
     .pipe(eslint.format({
       globals: [
@@ -32,6 +34,7 @@ gulp.task('lint', () => {
 gulp.task('scripts', ['lint'], () => {
   del(["static/js/**/*"])
   gulp.src('src/js/**/*.js')
+    .pipe(plumber())
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('static/js'));
@@ -41,12 +44,22 @@ gulp.task('scripts', ['lint'], () => {
 gulp.task('images', () => {
     del(['static/img/**/*']);
     gulp.src('src/img/**/*')
-        .pipe(gulp.dest('static/img'));
+      .pipe(plumber())
+      .pipe(gulp.dest('static/img'));
+});
+
+// Hash fonts
+gulp.task('fonts', () => {
+    del(['static/fonts/**/*']);
+    gulp.src('src/fonts/**/*')
+      .pipe(plumber())
+      .pipe(gulp.dest('static/fonts'));
 });
 
 gulp.task('sass', () => {
   del(["static/css/**/*"])
   gulp.src('src/scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sass({
 	outputStyle : 'compressed'
     }))
@@ -60,6 +73,7 @@ gulp.task('watch', () => {
   gulp.watch('src/scss/**/*', ['sass']);
   gulp.watch('src/js/**/*', ['scripts']);
   gulp.watch('src/img/**/*', ['images']);
+  gulp.watch('src/fonts/**/*', ['fonts']);
 });
 
-gulp.task('default', ['images', 'scripts', 'sass', 'watch']);
+gulp.task('default', ['images', 'fonts', 'scripts', 'sass', 'watch']);
